4328914d3b50e1ee45a40abb822ec26cf8b246d3
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-08-26 10:43:14 +0530

    Chor: No commit message

--- backend/controllers/page.controller.js
+++ backend/controllers/page.controller.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const headerHTML = fs.readFileSync(path.join(__dirname, "../../client/views/components/header.component.html"), "utf8");
 const indexHTML = fs.readFileSync(path.join(__dirname, "../../client/views/index.html"), "utf8");
 const homeHTML = fs.readFileSync(path.join(__dirname, "../../client/views/pages/home.html"), "utf-8");
+const addUserHTML = fs.readFileSync(path.join(__dirname, "../../client/views/pages/add-user.html"), "utf-8");
 
 function indexPage(req, res) {
   return res.end(indexHTML);
@@ -14,7 +15,7 @@ function homePage(req, res) {
 }
 
 function addUserPage(req, res) {
-
+  return res.end(addUserHTML.replace("{{HEADER}}", addUserHTML));
 }
 
 module.exports = {
