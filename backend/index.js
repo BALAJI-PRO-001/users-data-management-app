6ddd742d270ea1822ef2ba0344b4c73a9a079110
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-08-26 10:42:13 +0530

    Feat: Global error handler and admin router

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const pageRouter = require("./routes/page.route");
+const adminRouter = require("./routes/admin.route");
 const path = require("path");
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../client/public")));
 
+app.use("/api/v1/admin", adminRouter);
+
+app.use(express.static(path.join(__dirname, "../client/public")));
 app.use("/", pageRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode: statusCode,
+    message: message
+  });
+});
 
 const HOST = process.env.HOST || "localhost";
 const PROTOCOL = process.env.PROTOCOL || "http";
