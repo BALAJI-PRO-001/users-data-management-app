c04a2ba79fd81c0a2ae769e209f7e8428e6b567c
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-08-26 10:10:16 +0530

    Chor: No commit message

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const pageRouter = require("./routes/page.route");
+const path = require("path");
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client/public")));
 
 app.use("/", pageRouter);
 
