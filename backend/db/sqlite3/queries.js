9d01f337062cb7c17544b2d08e614f3352f7b195
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-08-30 21:29:52 +0530

    Added: create table sql

--- backend/db/sqlite3/queries.js
+++ backend/db/sqlite3/queries.js
@@ -1,5 +1,15 @@
 
 const CREATE_USERS_TABLE_SQL = `CREATE TABLE IF NOT EXISTS USERS (
                                   ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                                  
+                                  NAME VARCHAR(50) NOT NULL,
+                                  PHONE_NO INT UNIQUE NOT NULL,
+                                  ADDRESS VARCHAR(150) NOT NULL,
+                                  COW_NAME VARCHAR(80) NOT NULL,
+                                  BREED VARCHAR(80) NOT NULL,
+                                  BULL_NAME VARCHAR(80) NOT NULL,
+                                  AI_DATE VARCHAR(80) NOT NULL,
+                                  INJECTION_COST VARCHAR(50) NOT NULL
+                                  DATE_AND_TIME DATE 
                                )`;
+
+                            
