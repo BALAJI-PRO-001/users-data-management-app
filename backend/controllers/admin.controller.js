044980f334201912daaf18599ad98dc855c15a8b
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-09-12 10:15:27 +0530

    Bug: createNewUser when fetch inserted data

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -34,7 +34,8 @@ async function login(req, res, next) {
 async function createNewUser(req, res, next) {
   try {
     const {name, phoneNumber, address, cowName, cowBreed, bullName, aiDate, injectionCost} = req.body;
-    await User.addNewUser(name, phoneNumber, address, cowName, cowBreed, bullName, aiDate, injectionCost);
+    const newUser = await User.addNewUser(name, phoneNumber, address, cowName, cowBreed, bullName, aiDate, injectionCost);
+    console.log(newUser);
     res.status(201).json({
       success: true,
       statusCode: 201,
